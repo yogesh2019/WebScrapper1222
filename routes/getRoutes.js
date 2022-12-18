1debc887f9341afc43ecc494f80b6a9bd2c841f9
yogesh2019 <yogesh.khichi@gmail.com>
2022-12-18 15:42:06 +0530

    scrapping data into jobData using evaluate which takes param after a func

--- routes/getRoutes.js
+++ routes/getRoutes.js
@@ -1,15 +1,15 @@
 const express = require('express');
-const { mainModule } = require('process');
-
+const scrape = require("../scrapeFn/scrape");
 const routes = express.Router();
 
 routes.get('/indeed', async (req, res) => {
     try {
-        const { skill } = req.body;
-        let scrp = await main(skill);
+        let { skill } = req.body;
+        skill = 'sde';
+        let scrp = await scrape(skill);
         return res.status(200).send({
             status: "ok",
-            list: scrp?.list || {l:"l"}
+            list: scrp?.list || { l: "l" }
         })
     } catch (e) {
         console.log(e);
--- scrapeFn/scrape.js
+++ scrapeFn/scrape.js
@@ -2,10 +2,29 @@ const puppeteer = require('puppeteer');
 const data = {
     list: []
 };
-async function main(skill) {
+async function selectData(data) {
+    const items = document.querySelectorAll('td.resultContent');
+    const firstTestItem = items[0].firstElementChild.textContent;
+    data.list.push(firstTestItem);
+    return data;
+}
+async function scrape(skill) {
     const browser = await puppeteer.launch({ headless: false });
+    //geting search and removing the from part till &
+    const page = await browser.newPage();
+    const options = {
+        timeout: 0,
+        waituntil: 'networkidle0'
+    };
+
+    await page.goto(`https://in.indeed.com/jobs?q=${skill}&l=`, options);
+    const jobData = await page.evaluate(selectData, data);
+    let response = await jobData;
+    console.log(jobData.list[0]);
     browser.close();
+
 }
 
 
-module.exports = main;
+
+module.exports = scrape;
