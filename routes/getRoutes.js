2d0629204200f90ce77cb74506964d646918a868
yogesh2019 <yogesh.khichi@gmail.com>
2022-12-18 09:43:08 +0530

    adding indeed api which scraps data by main function yet to be defined

--- routes/getRoutes.js
+++ routes/getRoutes.js
@@ -1,9 +1,20 @@
 const express = require('express');
+const { mainModule } = require('process');
 
 const routes = express.Router();
 
-routes.get('/indeed', (req, res) => {
-    res.json({ success: "success" })
+routes.get('/indeed', async (req, res) => {
+    try {
+        const { skill } = req.body;
+        let scrp = await main(skill);
+        return res.status(200).send({
+            status: "ok",
+            list: scrp?.list || {l:"l"}
+        })
+    } catch (e) {
+        console.log(e);
+        return res.status(500).send(e);
+    }
 });
 
-module.exports =  routes ;
+module.exports = routes;
